df6d9da7ac353704be7827d144000223df636e25
Rick de Klerk <r.deklerk@fontys.nl>
2021-12-04 18:35:18 +0100

    Start of day 4

--- advent/day_3.py
+++ advent/day_3.py
@@ -2,18 +2,22 @@ from collections import Counter
 from statistics import mode
 from pathlib import Path
 from itertools import zip_longest
+from typing import Optional
 
 
 def read_diagnostics(path: Path) -> list[str]:
+    """Read a submarine support system diagnostics file."""
     with open(path, "r") as file:
         return file.read().split()
 
 
 def parse_bits(data: list[str]) -> list[list]:
+    """Parse bits from a list of string to a list of lists of strings."""
     return [[bit for bit in bits] for bits in data]
 
 
 def transpose(data: list[list]) -> list[list]:
+    """Transpose a list of lists."""
     return [list(col) for col in zip_longest(*data)]
 
 
@@ -23,86 +27,68 @@ def bits_to_int(bits: list, base: int = 2) -> int:
 
 
 def flip(bit: str) -> str:
+    """Flip a string "0" bit to a string "1" bit or the other way around."""
     return "0" if bit == "1" else "1"
 
 
-def most_common_bit(data: list[list]) -> list[str]:
-    """Returns the most common bit ("0" or "1"). In the case of a tie, returns "1"."""
+def most_common_bit(data: list[list], tie_breaker: str = "1") -> list[str]:
+    """Returns the most common bit ("0" or "1"). In the case of a tie, returns tie_breaker (default "1")."""
     counts = [Counter(row) for row in data]
 
     most_common_bits = []
     for count in counts:
         most_common = count.most_common(1)[0][0]
-        if most_common == "0" and count["0"] == count["1"]:  # in case of a tie
-            most_common = "1"
+        if count["0"] == count["1"]:  # in case of a tie
+            most_common = tie_breaker
         most_common_bits.append(most_common)
     return most_common_bits
 
 
 def calculate_power_consumption(data: list[list]) -> int:
+    """Calculate the submarine power consumption through the gamma rate and epsilon rate."""
     transposed = transpose(data)
     most_frequent = [mode(row) for row in transposed]
     gamma_rate = bits_to_int(most_frequent)
+
     least_frequent = [flip(bit) for bit in most_frequent]
     epsilon_rate = bits_to_int(least_frequent)
     return gamma_rate * epsilon_rate
 
 
-# def find_generator_rating(data: list[list]) -> list[str]:
-#     remaining = []
-#     last = []
-#
-#     for idx, col in enumerate(pattern):
-#         for row in data:
-#             if row[idx] == col:
-#                 last = row
-#                 remaining.append(last)
-#         if len(remaining) == 1:
-#             return remaining
-#         data = remaining
-#         print(remaining)
-#         remaining = []
-#     return remaining if remaining else last
-
-
-def find_generator_rating(data: list[list], invert: bool = False) -> list[str]:
+def find_generator_rating(data: list[list], invert: bool = False) -> Optional[list[str]]:
     remaining = transpose(data)
-    keep = []
     for idx, _ in enumerate(remaining):
         keep = []
         to_examine = remaining[idx]
         most_frequent = most_common_bit([to_examine])[0]
         if invert:
             most_frequent = "0" if most_frequent == "1" else "1"
-        print(f"\nMost frequent bit {most_frequent} on index {idx}")
         for row in transpose(remaining):
             if row[idx] == most_frequent:
                 keep.append(row)
-                print(f"Keeping: {row}")
         if len(keep) == 1:
-            print("gottem")
-            break
+            return keep[0]
         remaining = transpose(keep)
-    return keep[0]
 
 
 def calculate_life_support(data: list[list]) -> int:
-    data = read_diagnostics(Path("./data/day_3_data.txt"))
-    data = parse_bits(data)
+    """Calculate the life support rating through the oxygen generator and scrubber rating."""
     closest = find_generator_rating(data)
     oxygen_generator_rating = bits_to_int(closest)
 
     closest = find_generator_rating(data, True)
     scrubber_rating = bits_to_int(closest)
-    oxygen_generator_rating * scrubber_rating
-    return 1
+    return oxygen_generator_rating * scrubber_rating
 
 
 def main():
-    report = read_diagnostics(Path("./data/day_3_data.txt"))
+    report = read_diagnostics(Path("../data/day_3_data.txt"))
     report = parse_bits(report)
     power_consumption = calculate_power_consumption(report)
     print(f"The power consumption is equal to {power_consumption} power consumption units!")
+
+    life_support = calculate_life_support(report)
+    print(f"The life support rating is {life_support}!")
 
 
 if __name__ == "__main__":
---
+++ advent/day_4.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+class BingoCard:
+    pass
+
+
+def read_puzzle_input(path: Path) -> tuple[list[int], list[BingoCard]]:
+    pass
+
+
+def main():
+    numbers, boards = read_puzzle_input(Path("../data/day_4_example.txt"))
+
+
+if __name__ == "__main__":
+    main()
